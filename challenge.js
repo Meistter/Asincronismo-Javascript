abdb5014a99326efb6fb0e89fdc317b292da9acc
Josue Perez <josueperez723@gmail.com>
2022-12-06 21:13:21 -0400

    challenge add

---
+++ challenge.js
@@ -0,0 +1,40 @@
+// Primero debemos declarar e importar el paquete de XMLHttpRequest,
+const XMLHttpRequest = require('xmlhttprequest')
+// Declaramos como constante el url de la API:
+const API = 'https://api.escuelajs.co/api/v1'
+
+// Definimos la funcion principal
+
+function fetchData(urlApi, callback) {
+}
+// El primer parametro es la url de la api que trabajaremos, el segundo es la funcion a ejecutar
+
+// Debemos ahora instanciar el objeto XMLHttpRequest() en una variable
+let xhttp = new XMLHttpRequest();
+
+// A este objeto llamamos la propiedad Open para poder acceder y manipular la informacion de la API
+
+xhttp.open('GET', urlApi, true);
+
+// El primer elemento  es el tipo de solicitud que haremos, el segundo la URL de la api y el tercero es un valor
+// booleano que representa si usaremos o no asincronismo
+
+// Una vez hecho esto usaremos una funcion anonima para verificar si hubo error, para esto usamos la funcion:
+
+xhttp.onreadystatechange = function (e) { //Esta funcion captura el readyState cada vez que cambia (codigo 0-4)
+    // El ready state cambia desde el cero hasta el 4, cuando llega a 4 significa que terminó de procesar
+
+// por eso aqui preguntamos si ya el codigo es 4, en caso de ser 4 preguntamos si el status es 200(exitoso)
+if (xhttp.readyState === 4) {
+	if (xhttp.status === 200) {
+		callback(null, JSON.parse(xhttp.responseText)); //el primer argumento es en caso de error, pero como ya validamos eso
+        // lo dejamos en null, el segundo para
+        // parsear la respuesta a un formato usable (json en este caso)
+	} 
+} else  { //Si el codigo no fue exitoso (200) entonces
+		const error = new Error('error' + urlApi); //declaramos una constante error con una instancia de Error
+		return callback(error, null); //Le pasamos al callback el error y en la parte de los datos null
+}
+}
+// Una vez validada y lista nuestra funcion procedemos a enviar la peticion
+xhttp.send();
